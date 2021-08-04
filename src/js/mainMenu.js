87f7aa77cec55fa0e2859c1fcd4c24f54d4d656f
Ryan Ong <ryan.ong.jobs@gmail.com>
2021-08-04 16:47:29 -0700

    title screen animation

--- src/js/mainMenu.js
+++ src/js/mainMenu.js
@@ -2,13 +2,12 @@
 import { Scene } from 'phaser';
 import background from '../assets/backgrounds/menu_background.png'
 import mainTheme from '../assets/music/Main_Theme.mp3'
-import retroFont from '../assets/bitmaps/knight3.png'
+import greenRetroFont from '../assets/bitmaps/knight3_green.png'
+import redRetroFont from '../assets/bitmaps/knight3_red.png'
 import LevelScene from './level.js';
 
 const ROOM_WIDTH = 960;
 const ROOM_HEIGHT = 640;
-const LOGO_COLOR = 'lime';
-const LOGO_STROKE = 'blue';
 
 class MainMenu extends Phaser.Scene {
     // Initialization
@@ -23,7 +22,8 @@ class MainMenu extends Phaser.Scene {
     preload() {
         this.load.image('menu_background', background);
         this.load.audio('main_theme', mainTheme);
-        this.load.image('retro_font', retroFont);
+        this.load.image('green_retro_font', greenRetroFont);
+        this.load.image('red_retro_font', redRetroFont);
     }
 
     create() {
@@ -34,38 +34,78 @@ class MainMenu extends Phaser.Scene {
         this.add.image(0, 0, 'menu_background').setOrigin(0, 0);
 
         // Logo
-        var fontConfig = {
-            image: 'retro_font',
+        var titleFontConfig = {
+            image: 'green_retro_font',
             width: 31,
             height: 25,
             chars: Phaser.GameObjects.RetroFont.TEXT_SET6,
             charsPerRow: 10,
             spacing: { x: 1, y: 1 }
         };
-        this.cache.bitmapFont.add('retro_font', Phaser.GameObjects.RetroFont.Parse(this, fontConfig));
-        this.dynamic = this.add.bitmapText(100, 100, 'retro_font', 'MARS MAYHEM!');
-        this.dynamic.setScale(2);
+        this.cache.bitmapFont.add('retro_font', Phaser.GameObjects.RetroFont.Parse(this, titleFontConfig));
+        this.dynamic = this.add.bitmapText(200, 100, 'retro_font', ' MARS\nMAYHEM');
+        this.dynamic.setScale(3);
 
-        // Clicking start text moves player to first level
-        this.start_option = this.add.text(420, 360, 'Start', {
-            fontFamily: 'Verdana',
-            fontSize: '48px',
-            fontStyle: 'bold',
-            color: LOGO_COLOR,
-            stroke: LOGO_STROKE,
-            strokeThickness: '4'
+        // --------------------------------------------------------
+        // Clicking flashing start text moves player to first level
+        // --------------------------------------------------------
+        // Adding bitmap text to scene
+        var startFontConfig = {
+            image: 'red_retro_font',
+            width: 31,
+            height: 25,
+            chars: Phaser.GameObjects.RetroFont.TEXT_SET6,
+            charsPerRow: 10,
+            spacing: { x: 1, y: 1 }
+        };
+        this.cache.bitmapFont.add('retro_font', Phaser.GameObjects.RetroFont.Parse(this, startFontConfig));
+        this.start_option = this.add.bitmapText(250, 475, 'retro_font', ' START!');
+        this.start_option.setScale(2);
+        
+        // Fade in/out animation
+        this.start_tween = this.tweens.timeline({
+            tweens: [
+                {   
+                    // Starts invisible
+                    targets: this.start_option, easing: 'Linear', duration: 0,  alpha: 0
+                },
+                {   
+                    // Fades to visible
+                    targets: this.start_option, easing: 'Linear', duration: 1000,  alpha: 1
+                },
+                {
+                    // Holds visibility
+                    targets: this.start_option, easing: 'Linear', duration: 500,  alpha: 1
+                },
+                {
+                    // Returns back to invisible
+                    targets: this.start_option, easing: 'Linear', duration: 1000,  alpha: 0
+                }
+            ],
+            loop: -1
         });
+        
+        // Clicking action
         this.start_option.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.start_option.width, this.start_option.height), Phaser.Geom.Rectangle.Contains);
         this.start_option.on('pointerdown', function() {
             music.stop();
             this.scene.start('Level 1');
         }, this);
+        
 
         // Click on a spot to print x/y coordinates to console.
         this.input.on('pointerdown', function (pointer) {
             console.log(pointer.x, pointer.y);
         });
 
+        // Credit text
+        this.add.text(320, 570, 'Created by Devin Leung, Alex Munox, and Ryan Ong', {
+            fontFamily: 'Arial',
+            color: 'Black',
+            fontSize: 14
+        });
+
+        // Main menu music
         let music = this.sound.add('main_theme')
         music.setVolume(0.5);
         music.setLoop(true);
